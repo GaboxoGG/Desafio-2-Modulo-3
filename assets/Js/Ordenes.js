3e2d6e13d0370dc8833ec7f23960d35e0d6c16d8
GaboxoGG <gabriel.arriagada.b@gmail.com>
2026-03-01 01:24:45 -0300

    avanzando...

--- assets/Js/Ordenes.js
+++ assets/Js/Ordenes.js
@@ -11,3 +11,22 @@ boton.addEventListener('click', alternarBorde)
 imagen.addEventListener('click', alternarBorde);
 
 //Parte 2
+
+
+//Parte 3
+function verificar() {
+    const N1 = document.getElementById("Cantidad1").Value;
+    const N2 = document.getElementById("Cantidad2").Value;
+    const N3 = document.getElementById("Cantidad3").Value;
+
+    const Contraseña = N1 + N2 + N3;
+    const Resultado = document.getElementById("Resultado");
+
+    if (Contraseña === "911") {
+        Resultado.textContent = "Contraseña 1 Correcta";
+    } else if (Contraseña === "714") {
+        Resultado.textContent = "Contraseña 2 Correcta";
+    } else {
+        Resultado.textContent = "Contraseña Incorrecta"
+    }
+}
