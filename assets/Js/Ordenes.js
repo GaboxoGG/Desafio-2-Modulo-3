d89ec0c1771c2b2325eebcbc090b7257e4402a83
GaboxoGG <gabriel.arriagada.b@gmail.com>
2026-03-01 08:50:46 -0300

    2 de 3

--- assets/Js/Ordenes.js
+++ assets/Js/Ordenes.js
@@ -15,24 +15,27 @@ imagen.addEventListener('click', alternarBorde);
 
 //Parte 3
 for (let i = 1; i <= 9; i++) {
-    document.getElementById("Cantidad1").innerHTML += `<option value="${i}">${i}</option>`
-    document.getElementById("Cantidad2").innerHTML += `<option value="${i}">${i}</option>`
-    document.getElementById("Cantidad3").innerHTML += `<option value="${i}">${i}</option>`
+  document.getElementById("Cantidad1").innerHTML += `<option value="${i}">${i}</option>`;
+  document.getElementById("Cantidad2").innerHTML += `<option value="${i}">${i}</option>`;
+  document.getElementById("Cantidad3").innerHTML += `<option value="${i}">${i}</option>`;
 }
 
 function verificar() {
-    const N1 = document.getElementById("Cantidad1").Value;
-    const N2 = document.getElementById("Cantidad2").Value;
-    const N3 = document.getElementById("Cantidad3").Value;
+  const N1 = document.getElementById("Cantidad1").value;
+  const N2 = document.getElementById("Cantidad2").value;
+  const N3 = document.getElementById("Cantidad3").value;
 
-    const Contraseña = N1 + N2 + N3;
-    const Resultado = document.getElementById("Resultado");
+  const password = N1 + N2 + N3;
+  const resultado = document.getElementById("Resultado");
 
-    if (Contraseña === "911") {
-        Resultado.textContent = "Contraseña 1 Correcta";
-    } else if (Contraseña === "714") {
-        Resultado.textContent = "Contraseña 2 Correcta";
-    } else {
-        Resultado.textContent = "Contraseña Incorrecta"
-    }
+  if (password === "911") {
+    resultado.textContent = "Password 1 correcto";
+    
+  } else if (password === "714") {
+    resultado.textContent = "Password 2 es correcto";
+    
+  } else {
+    resultado.textContent = "Password incorrecto";
+    
+  }
 }
